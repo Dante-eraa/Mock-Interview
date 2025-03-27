24debf777ebf4c43e79c25b78aa41e49ab221e0a
Dante-eraa <eziokenwayearth@gmail.com>
2025-03-27 19:23:09 +0530

    Add SignIn and SignUp routes with corresponding components

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import PublicLayout from "@/layouts/PublicLayout"
 import Home from "@/routes/Home"
 import AuthLayout from "@/layouts/AuthLayout"
+import SignIn from "./routes/SignIn"
+import SignUp from "./routes/SignUp"
 
 
 const App = () => {
@@ -14,7 +16,8 @@ const App = () => {
           </Route>
           {/*Authentication Layout*/}
           <Route element={<AuthLayout />}>
-            <Route path="/auth" element={<Home />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
           </Route>
         </Routes>
       </Router>
