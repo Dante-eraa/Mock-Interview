f4165c451331a63485232def7c4a2a4980c67149
Dante-eraa <eziokenwayearth@gmail.com>
2025-03-28 22:19:59 +0530

    Add Firebase configuration and AuthHandler component; implement user data storage on sign-in

--- src/layouts/PublicLayout.tsx
+++ src/layouts/PublicLayout.tsx
@@ -1,10 +1,12 @@
 import Footer from "@/components/Footer"
+import AuthHandler from "@/components/handlers/AuthHandler"
 import Header from "@/components/Header"
 import { Outlet } from "react-router-dom"
 
 const PublicLayout = () => {
     return (
         <div className="w-full">
+            <AuthHandler />
             <Header />
             <Outlet />
             <Footer />
