4300bc2134efa32a8eec91b958b60afee8a9c952
Dante-eraa <eziokenwayearth@gmail.com>
2025-03-29 10:05:14 +0530

    Add Separator component and integrate it into Dashboard; update styles and fix CSS syntax

--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -1,4 +1,9 @@
 import Headings from "@/components/Headings";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+
+import { Plus } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const Dashboard = () => {
   return (
@@ -8,7 +13,14 @@ const Dashboard = () => {
           title="Dashboard"
           description="Create and start your AI Mock Interview"
         />
+
+        <Link to={"/generate/create"}>
+          <Button size={"sm"}>
+            <Plus /> Add Now
+          </Button>
+        </Link>
       </div>
+      <Separator className="my-4" />
     </>
   );
 };
