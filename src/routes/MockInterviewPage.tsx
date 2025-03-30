6cb120a0a856413871892a4642257b12a19cbd98
Dante-eraa <eziokenwayearth@gmail.com>
2025-03-30 12:43:41 +0530

    Add Alert component to MockInterviewPage for user guidance on enabling webcam and microphone

--- src/routes/MockInterviewPage.tsx
+++ src/routes/MockInterviewPage.tsx
@@ -7,8 +7,9 @@ import { Link, Outlet, useNavigate, useParams } from "react-router-dom";
 import LoaderPage from "./LoaderPage";
 import CustomBreadcrum from "@/components/CustomBreadcrum";
 import { Button } from "@/components/ui/button";
-import { ArrowBigRight } from "lucide-react";
+import { ArrowBigRight, Lightbulb, Mic, Video } from "lucide-react";
 import InterviewPin from "@/components/InterviewPin";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
 const MockInterviewPage = () => {
   const { interviewId } = useParams<{ interviewId: string }>();
@@ -64,6 +65,24 @@ const MockInterviewPage = () => {
         </Link>
       </div>
       {interview && <InterviewPin interview={interview} onMockPage />}
+      <Alert className="bg-sky-100 border-sky-500 text-sky-600">
+        <div className="flex items-center gap-3 mb-2">
+          <Lightbulb className="w-4 h-4 text-sky-900" />
+          <AlertTitle className="text-sky-900">
+            Important Information
+          </AlertTitle>
+        </div>
+        <div className="flex items-center gap-2 px-2 py-2">
+          <Video className="text-sky-600 w-5 h-5" />
+          <AlertTitle className="text-sky-600">Enable Webcam &</AlertTitle>
+          <Mic className=" text-sky-600 w-5 h-5" />
+          <AlertTitle className="text-sky-600">Microphone</AlertTitle>
+        </div>
+        <AlertDescription className="mt-3">
+          To proceed with the AI mock interview, please allow access to your
+          webcam and microphone.
+        </AlertDescription>
+      </Alert>
     </div>
   );
 };
