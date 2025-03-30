59d7509c74eb8688908bd65a0c13df46b6b7a2db
Dante-eraa <eziokenwayearth@gmail.com>
2025-03-30 08:35:22 +0530

    Implement InterviewPin component with loading state and add Card and Badge UI components

--- src/components/InterviewPin.tsx
+++ src/components/InterviewPin.tsx
@@ -1,12 +1,49 @@
 import { Interview } from "@/types";
+import { useAuth } from "@clerk/clerk-react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Card, CardDescription, CardFooter, CardTitle } from "./ui/card";
+import { Badge } from "./ui/badge";
+import { cn } from "@/lib/utils";
 
 interface InterviewPinProps {
   interview: Interview;
   onMockPage?: boolean;
 }
 
-const InterviewPin = ({ interview }: InterviewPinProps) => {
-  return <div></div>;
+const InterviewPin = ({ interview, onMockPage = false }: InterviewPinProps) => {
+  const navigate = useNavigate();
+
+  const [loading, setLoading] = useState(false);
+  const { userId } = useAuth();
+
+  return (
+    <Card className="p-4 rounded-md shadow-none hover:shadow-md shadow-gray-300 cursor-pointer transition-all space-y-4">
+      <CardTitle className="text-lg">{interview?.position}</CardTitle>
+      <CardDescription className="text-xs">
+        {interview?.description}
+      </CardDescription>
+      <div className="w-full flex items-center gap-3 flex-wrap">
+        {interview.techStack.split(",").map((eachword, id) => {
+          return (
+            <Badge
+              variant={"outline"}
+              key={id}
+              className="text-xs text-muted-foreground hover:text-sky-900 hover:border-sky-100 hover:bg-sky-400"
+            >
+              {eachword}
+            </Badge>
+          );
+        })}
+      </div>
+      <CardFooter
+        className={cn(
+          "w-full flex items-center p-0",
+          onMockPage ? "justify-end" : "justify-between"
+        )}
+      ></CardFooter>
+    </Card>
+  );
 };
 
 export default InterviewPin;
