a1aba7f8f62dc44f11e55284234863f1b7afdfa9
Dante-eraa <eziokenwayearth@gmail.com>
2025-03-30 13:00:57 +0530

    Add webcam functionality to MockInterviewPage with user alerts for enabling devices

--- src/routes/MockInterviewPage.tsx
+++ src/routes/MockInterviewPage.tsx
@@ -3,13 +3,14 @@ import { Interview } from "@/types";
 import { doc, getDoc } from "firebase/firestore";
 
 import { useEffect, useState } from "react";
-import { Link, Outlet, useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import LoaderPage from "./LoaderPage";
 import CustomBreadcrum from "@/components/CustomBreadcrum";
 import { Button } from "@/components/ui/button";
-import { ArrowBigRight, Lightbulb, Mic, Video } from "lucide-react";
+import { ArrowBigRight, Lightbulb, Mic, Video, WebcamIcon } from "lucide-react";
 import InterviewPin from "@/components/InterviewPin";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import Webcam from "react-webcam";
 
 const MockInterviewPage = () => {
   const { interviewId } = useParams<{ interviewId: string }>();
@@ -69,7 +70,7 @@ const MockInterviewPage = () => {
         <div className="flex items-center gap-3 mb-2">
           <Lightbulb className="w-4 h-4 text-sky-900" />
           <AlertTitle className="text-sky-900">
-            Important Information
+            AI Mock Interview Alert
           </AlertTitle>
         </div>
         <div className="flex items-center gap-2 px-2 py-2">
@@ -79,10 +80,31 @@ const MockInterviewPage = () => {
           <AlertTitle className="text-sky-600">Microphone</AlertTitle>
         </div>
         <AlertDescription className="mt-3">
-          To proceed with the AI mock interview, please allow access to your
-          webcam and microphone.
+          To get the best experience in your AI mock interview, please enable
+          your webcam and microphone. Ensuring both devices are active will
+          allow smooth communication and real-time analysis of your responses.
+          This will help provide accurate feedback and improve your interview
+          preparation effectively.
         </AlertDescription>
       </Alert>
+      <div className="flex items-center justify-center w-full h-full">
+        <div className="w-full h-[400px] md:w-96 flex flex-col items-center justify-center border bg-gray-50 rounded-md">
+          {isWebCamEnabled ? (
+            <Webcam
+              onUserMedia={() => setIsWebCamEnabled(true)}
+              onUserMediaError={() => setIsWebCamEnabled(false)}
+              className="w-full h-full object-cover rounded-md"
+            />
+          ) : (
+            <WebcamIcon className="min-w-24 min-h-24 text-muted-foreground" />
+          )}
+        </div>
+      </div>
+      <div className="flex items-center justify-center">
+        <Button onClick={() => setIsWebCamEnabled(!isWebCamEnabled)}>
+          {isWebCamEnabled ? "Disable Webcam" : "Enable Webcam"}
+        </Button>
+      </div>
     </div>
   );
 };
