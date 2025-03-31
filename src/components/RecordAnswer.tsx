ec41cc29e643955ebdceff152956835960228fae
Dante-eraa <eziokenwayearth@gmail.com>
2025-03-31 12:12:20 +0530

    Enhance RecordAnswer component: add functionality to record answers again and save results, improve transcript handling

--- src/components/RecordAnswer.tsx
+++ src/components/RecordAnswer.tsx
@@ -2,11 +2,12 @@ import { Interview } from "@/types";
 import { useAuth } from "@clerk/clerk-react";
 import { Tooltip } from "@radix-ui/react-tooltip";
 import { useState } from "react";
-import useSpeechToText from "react-hook-speech-to-text";
+import useSpeechToText, { ResultType } from "react-hook-speech-to-text";
 import { useParams } from "react-router-dom";
 import { TooltipButton } from "./ToolTipButton";
-import { CircleStop, Mic, Webcam } from "lucide-react";
+import { CircleStop, Loader, Mic, RefreshCcw, Save } from "lucide-react";
 import { toast } from "sonner";
+import { useEffect } from "react";
 interface RecordAnswerProps {
   question: { question: string; answer: string };
 }
@@ -41,16 +42,32 @@ const RecordAnswer = ({ question }: RecordAnswerProps) => {
   const recordUserAnswer = async () => {
     if (isRecording) {
       stopSpeechToText();
-    }
-    if (userAnswer?.length < 30) {
-      toast.error("Error", {
-        description: "Your answer should be more than 30 characters",
-      });
-      return;
+      if (userAnswer?.length < 30) {
+        toast.error("Error", {
+          description: "Your answer should be more than 30 characters",
+        });
+        return;
+      }
     } else {
       startSpeechToText();
     }
   };
+
+  const recordNewAnswer = () => {
+    setUserAnswer("");
+    stopSpeechToText();
+    startSpeechToText();
+  };
+
+  useEffect(() => {
+    const combineTranscripts = results
+      .filter((result): result is ResultType => typeof result !== "string")
+      .map((result) => result.transcript)
+      .join(" ");
+    setUserAnswer(
+      combineTranscripts + (interimResult ? ` ${interimResult}` : "")
+    );
+  }, [results, interimResult]);
 
   return (
     <div className="w-full flex flex-col items-center gap-8 mt-4">
@@ -66,6 +83,33 @@ const RecordAnswer = ({ question }: RecordAnswerProps) => {
           }
           onClick={recordUserAnswer}
         />
+
+        <TooltipButton
+          content="Record Again"
+          icon={<RefreshCcw className="min-w-5 min-h-5" />}
+          onClick={recordNewAnswer}
+        />
+        <TooltipButton
+          content="Save Result"
+          icon={
+            isAIGenerating ? (
+              <Loader
+                className="min-w-5
+                min-h-5 animate-spin"
+              />
+            ) : (
+              <Save className="min-w-5 min-h-5" />
+            )
+          }
+          onClick={() => setOpen(!open)}
+          disbaled={!aiResult}
+        />
+      </div>
+      <div className="w-full mt-4 p-4 border rounded-md bg-gray-50">
+        <h2 className="text-lg font-semibold"> Your Answer</h2>
+        <p className="whitespace-normal text-sm mt-2 text-gray-700">
+          {userAnswer || "Start recording to see your answer here"}
+        </p>
       </div>
     </div>
   );
